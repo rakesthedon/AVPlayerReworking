84a484d399fd62bbbff111e872f4b2aed64f0963
Thomas Jacques, Yannick <ythomas@nuglif.com>
2019-12-26 16:26:52 -0500

    Initial Commit

---
+++ AVPlayerReworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayerReworking
+//
+//  Created by Yannick Jacques on 2019-12-26.
+//  Copyright © 2019 Yannick Jacques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
